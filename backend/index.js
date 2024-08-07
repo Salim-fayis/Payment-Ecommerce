f20328c7d5b98eb635eba9d9db4c0457dc6603ba
Salim-fayis <111036330+Salim-fayis@users.noreply.github.com>
2024-08-07 19:55:34 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -24,12 +24,12 @@ app.use(cookieParser());
 
 
 app.use("/api", router);
+// const __dirname = path.resolve()
 
-
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
