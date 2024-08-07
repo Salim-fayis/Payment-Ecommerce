75579db8a75dbdca64a6ae546d908d8c46d30b23
Salim-fayis <111036330+Salim-fayis@users.noreply.github.com>
2024-08-07 20:19:01 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use("/api", router);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
