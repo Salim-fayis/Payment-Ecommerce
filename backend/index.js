f49068ba7c9975888ecf0b774c1d4bc0b09245f4
Salim-fayis <111036330+Salim-fayis@users.noreply.github.com>
2024-08-20 18:29:41 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const path = require('path');
-
 require('dotenv').config();
 const connectDB = require('./config/db');
 const router = require('./routes');
@@ -22,22 +20,15 @@ app.use(cookieParser());
 // API routes
 app.use("/api", router);
 
-// Serve static files from the frontend build
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 
 // Start server
-const PORT = process.env.PORT || 8080;
+const PORT = 8080 || process.env.PORT
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Connected to DB");
-        console.log("Server is running on port " + PORT);
-    });
-}).catch(err => {
-    console.error("Failed to connect to DB", err);
-});
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
